beb861a26401260f0c62ea51db3aa85e4f0c82fe
Travailleur Lorick <travailleurlorick@MacBook-Pro-de-Travailleur.local>
2019-03-07 17:43:35 +0100

    add my file

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is my CLI resume.
+
+### How is it work ?
+
+- Install the package globally  `npm install -g lorick-cli-resume`
+- Run it `lorick-resume`
+- Now you can navigate in my resume 🙂
---
+++ index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+const response = chalk.bold.green;
+
+const resume = require("./resume.json");
+
+const resumePrompts = {
+  type: "list",
+  name: "resumeOptions",
+  message: "What do you want to know about me?",
+  choices: [...Object.keys(resume), "Exit"]
+};
+
+const main = () => {
+  console.log("Hi, I'm Lorick and a French front-end developer");
+  resumeHandler();
+};
+
+const resumeHandler = () => {
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == "Exit") {
+      return;
+    }
+    const option = answer.resumeOptions;
+    console.log(response("--------------------------------------"));
+    resume[`${option}`].forEach(info => {
+      console.log(response("|   => " + info));
+    });
+    console.log(response("--------------------------------------"));
+    // console.log(resume[`${option}`]);
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.exitBack == "Back") {
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+};
+
+main();
